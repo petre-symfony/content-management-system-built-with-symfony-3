14b2c5a8e037471bcb8d80e4dd3a5863536bc075
petrero <petrero@gmail.com>
2017-07-10 17:43:17 +0300

    Retrieve posts data and display them on the page

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -4,7 +4,25 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use AppBundle\Entity\Post;
 
-class PostController extends Controller
-{
+class PostController extends Controller {
+  /**
+   * @Route("/", name="homepage")
+   * @Method("GET")
+   */
+  public function listAction(){
+    $em = $this->getDoctrine()->getManager();
+    $repository = $em->getRepository('AppBundle:Post');
+    
+    $posts = $repository->findAll();
+    
+    return $this->render(
+      'post/list.html.twig',
+      array(
+        'posts' => $posts
+      )
+    );
+  }
 }
