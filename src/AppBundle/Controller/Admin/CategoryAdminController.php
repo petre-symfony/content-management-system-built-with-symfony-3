b2ac5a4382532c6291da43d4d3d600973ddad12b
petrero <petrero@gmail.com>
2017-07-20 23:13:24 +0300

    Create Admin Homepage

---
+++ src/AppBundle/Controller/Admin/CategoryAdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use AppBundle\Entity\Category;
+/**
+ * @Route("/admin")
+ */
+class CategoryAdminController extends Controller{
+  /**
+   * @Route("/", name="admin_category_list")
+   * @Method("GET")
+   */
+  public function listAction(){
+    return $this->render('admin/category/list.html.twig');
+  }
+}
