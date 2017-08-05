25785d66dbf42896d0e2458cd3eac8065ff7192a
petrero <petrero@gmail.com>
2017-08-05 03:07:27 +0300

    You can create a doctrine listener to automatically upload the file when persisting the entity. The listener can also create the File instance based on the path when fetching entities from the database.

---
+++ src/AppBundle/EventListener/PostImageUploadListener.php
@@ -0,0 +1,45 @@
+<?php
+namespace AppBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\File;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use AppBundle\Entity\Post;
+use AppBundle\Service\FileUploader;
+
+
+class PostImageUploadListener {
+  private $uploader;
+  
+  public function __construct(FileUploader $uploader) {
+    $this->uploader = $uploader;
+  }
+  
+  public function prePersist(LifecycleEventArgs $args){
+    $entity = $args->getEntity();
+    $this->uploadFile($entity);
+  }
+  
+  public function preUpdate(PreUpdateEventArgs $args) {
+    $entity = $args->getEntity();
+    $this->uploadFile($entity);
+  }
+  
+  private function uploadFile($entity){
+    //upload only works for Post entities
+    if(!$entity instanceof Post){
+      return;
+    }
+    
+    $file = $entity->getPostImage();
+    
+    //only upload new files
+    if(!$file instanceof UploadedFile){
+      return;
+    }
+    
+    $filename = $this->uploader->upload($file);
+    $entity->setPostImage($filename);
+  }
+}
