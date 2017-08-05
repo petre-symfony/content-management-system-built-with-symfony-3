bd8609da60a5208ccb2784c7a9afb6883255cf77
petrero <petrero@gmail.com>
2017-08-05 03:01:08 +0300

    Set the mime type of the uploaded file.

--- src/AppBundle/Entity/Post.php
+++ src/AppBundle/Entity/Post.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Post
@@ -40,6 +41,7 @@ class Post {
    * @var string
    *
    * @ORM\Column(name="post_image", type="string", length=255)
+   * @Assert\File(mimeTypes={ "image/jpeg" })
    */
   private $postImage;
 
